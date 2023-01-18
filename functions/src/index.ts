1074370714f677e5820a5552793c2174011b8fcd
rcheiko <rcheiko@student.42nice.fr>
2023-01-18 10:17:18 +0100

    update file

---
+++ functions/src/index.ts
@@ -0,0 +1,7 @@
+import { schedule_user_monthly_performance } from "./monthly_performance"
+import { schedule_user_portfolio } from "./portfolio"
+import { schedule_TVL } from "./tvl"
+
+exports.schedule_user_monthly_performance = schedule_user_monthly_performance
+exports.schedule_user_portfolio = schedule_user_portfolio
+exports.schedule_TVL = schedule_TVL
