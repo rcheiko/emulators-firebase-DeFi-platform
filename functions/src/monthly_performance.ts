8565d97bb141c134250f86e9dc8fdca78ea0c5be
rcheiko <rcheiko@student.42nice.fr>
2023-03-20 09:25:21 +0100

    fixed monthly_performance

--- functions/src/monthly_performance.ts
+++ functions/src/monthly_performance.ts
@@ -1,42 +1,22 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
-import { ethers } from "ethers";
-import * as lusdcTokenABI from "./abi/LUSDC.json";
-
 const db = admin.firestore();
-
-const providerLink =
-  "wss://polygon-mainnet.g.alchemy.com/v2/oCpKb4y16gIydWUO7n2flcYSKAzkH3_p";
-const lusdcTokenAddress = "0x1080fFf81F5DF23c024ce38D785a734Eb5f6d84d";
-const lusdcDecimals = 18;
 
 export const schedule_user_monthly_performance = functions
   .region("europe-west1")
   .pubsub.schedule("0 12 1 * *")
   .timeZone("Europe/Paris")
   .onRun(async () => {
-    const provider = new ethers.providers.WebSocketProvider(providerLink);
-    const ltyToken = new ethers.Contract(
-      lusdcTokenAddress,
-      lusdcTokenABI.abi,
-      provider
-    );
-
     const month: number = 60 * 60 * 24 * 30 * 1000;
 
     const userRef = db.collection("user");
     let allUser = await userRef.get();
 
-    let finalBalance = 0;
-    let address = ""; // address of user
-    let balanceTrx; // balance of all the transaction of the month
-    let balParsed = 0;
-    let profit = 0; // profit of the month
-    let pourcentage = 0; // pourcentage of profit of the month
-
     allUser.forEach(async (doc) => {
-      address = doc.id;
-      const user = await db
+      let finalBalance = 0;
+      let profit = 0; // profit of the month
+      let pourcentage = 0; // pourcentage of profit of the month
+      const userTrx = await db
         .collection("user")
         .doc(doc.id)
         .collection("transactions")
@@ -44,19 +24,24 @@ export const schedule_user_monthly_performance = functions
         .orderBy("created", "asc")
         .get();
 
-      user.forEach(async (doc) => {
-        finalBalance += doc.data().trx;
+      userTrx.forEach((docc) => {
+        finalBalance = finalBalance + docc.data().trx;
       });
 
       const result = await getBalanceLastMonth(doc);
       if (result) finalBalance += result;
 
-      balanceTrx = await ltyToken.balanceOf(address);
+      const lastPortfolioDoc = await db
+        .collection("user")
+        .doc(doc.id)
+        .collection("portfolio")
+        .orderBy("created", "desc")
+        .limit(1)
+        .get();
+      const balanceUser: number = lastPortfolioDoc.docs[0].data().balance;
 
-      balParsed = parseInt(balanceTrx._hex, 16) / 10 ** lusdcDecimals;
-
-      profit = balParsed - finalBalance;
-      pourcentage = profit / balParsed;
+      profit = balanceUser - finalBalance;
+      pourcentage = profit / balanceUser;
 
       await db
         .collection("user")
@@ -64,41 +49,28 @@ export const schedule_user_monthly_performance = functions
         .collection("monthlyPerformance")
         .doc()
         .set({
-          balance: balParsed,
+          finalBalance: finalBalance,
           profit: profit,
           pourcentage: pourcentage,
           created: Date.now(),
           month: new Date().getMonth(),
         });
-      finalBalance = 0;
     });
   });
 
 export const manual_user_monthly_performance = functions
   .region("europe-west1")
   .https.onRequest(async (req, res) => {
-    const provider = new ethers.providers.WebSocketProvider(providerLink);
-    const ltyToken = new ethers.Contract(
-      lusdcTokenAddress,
-      lusdcTokenABI.abi,
-      provider
-    );
-
     const month: number = 60 * 60 * 24 * 30 * 1000;
 
     const userRef = db.collection("user");
     let allUser = await userRef.get();
 
-    let finalBalance = 0;
-    let address = ""; // address of user
-    let balanceTrx; // balance of all the transaction of the month
-    let balParsed = 0;
-    let profit = 0; // profit of the month
-    let pourcentage = 0; // pourcentage of profit of the month
-
     allUser.forEach(async (doc) => {
-      address = doc.id;
-      const user = await db
+      let finalBalance = 0;
+      let profit = 0; // profit of the month
+      let pourcentage = 0; // pourcentage of profit of the month
+      const userTrx = await db
         .collection("user")
         .doc(doc.id)
         .collection("transactions")
@@ -106,19 +78,24 @@ export const manual_user_monthly_performance = functions
         .orderBy("created", "asc")
         .get();
 
-      user.forEach(async (doc) => {
-        finalBalance += doc.data().trx;
+      userTrx.forEach((docc) => {
+        finalBalance = finalBalance + docc.data().trx;
       });
 
       const result = await getBalanceLastMonth(doc);
       if (result) finalBalance += result;
 
-      balanceTrx = await ltyToken.balanceOf(address);
+      const lastPortfolioDoc = await db
+        .collection("user")
+        .doc(doc.id)
+        .collection("portfolio")
+        .orderBy("created", "desc")
+        .limit(1)
+        .get();
+      const balanceUser: number = lastPortfolioDoc.docs[0].data().balance;
 
-      balParsed = parseInt(balanceTrx._hex, 16) / 10 ** lusdcDecimals;
-
-      profit = balParsed - finalBalance;
-      pourcentage = profit / balParsed;
+      profit = balanceUser - finalBalance;
+      pourcentage = profit / balanceUser;
 
       await db
         .collection("user")
@@ -126,23 +103,15 @@ export const manual_user_monthly_performance = functions
         .collection("monthlyPerformance")
         .doc()
         .set({
-          balance: balParsed,
+          finalBalance: finalBalance,
           profit: profit,
           pourcentage: pourcentage,
           created: Date.now(),
           month: new Date().getMonth(),
         });
-      finalBalance = 0;
     });
     res.send("All user monthly performance has been added");
   });
-
-// Example : if it's january it will be 0 and minus 1 it will be 11
-const calcPrevMonthsInNumber = (month: number) => {
-  if (month === 0) return 11;
-  if (month === 11) return 0;
-  return month - 1;
-};
 
 // Get portfolio balance of the last months
 const getBalanceLastMonth = async (
@@ -155,8 +124,15 @@ const getBalanceLastMonth = async (
     .where("month", "==", calcPrevMonthsInNumber(new Date().getMonth()))
     .get();
   let res;
-  monthly_perf.forEach(async (doc) => {
-    res = doc.data().balance;
+  monthly_perf.forEach(async (docc) => {
+    res = docc.data().balance;
   });
   return res;
+};
+
+// Example : if it's january it will be 0 and minus 1 it will be 11
+const calcPrevMonthsInNumber = (month: number) => {
+  if (month === 0) return 11;
+  if (month === 11) return 0;
+  return month - 1;
 };
